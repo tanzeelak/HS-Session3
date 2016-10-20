ed039b37ba7e60c1a6ce255027868cf2939f86fe
tanzeelak <tnkhan8042@gmail.com>
2016-10-19 21:02:11 -0700

    hello

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+function sum(a, b) { return a + b; }
+function task(fn, a, b, cb) {
+    setTimeout(() => {
+	    cb(fn(a, b));
+	}, 1000);
+}
+
+task(sum, 5, 3, result => {
+	console.log(result);
+    });
